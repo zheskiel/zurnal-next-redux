083fc0103352f13360805e486735dd992dafe4e9
juanzheng <juanzheng@kkbox.com>
2022-06-20 01:52:32 +0800

    add intersection observer

--- Components/PostListItem/index.js
+++ Components/PostListItem/index.js
@@ -1,34 +1,60 @@
-import React, { Component } from "react";
+import React, { Component, createRef } from "react";
 
 import { CategoryLink, PostLink } from "../../utils/link-generator";
+import { ImgError } from '../../utils/helpers';
+
+import watchIntersection from '../../libs/intersection';
 
 class PostsListItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.imgRef = createRef();
+
+    this.state = {
+      isInView: false
+    }
+  }
+
+  componentDidMount() {
+    watchIntersection(this.imgRef, () => {
+      this.setState({
+        isInView: true,
+      });
+    });
+  }
+
   render() {
+    const { isInView } = this.state;
+
     const { elem } = this.props;
+    const { title, featured_image } = elem;
 
     return (
       <article>
-        <div className="meta-image">
+        <div className='meta-image'>
           <PostLink elem={elem}>
-            <a>
-              <img
-                className="img-fluid"
-                onError={(e) => ImgError(e)}
-                src={elem.featured_image}
-                alt={elem.title}
-              />
+            <a ref={this.imgRef}>
+              {isInView && (
+                <img
+                  className='img-fluid'
+                  onError={(e) => ImgError(e)}
+                  src={featured_image}
+                  alt={title}
+                />
+              )}
             </a>
           </PostLink>
         </div>
 
-        <header className="entry-header">
-          <span className="meta-category">
+        <header className='entry-header'>
+          <span className='meta-category'>
             <CategoryLink elem={elem}>{elem.category.name}</CategoryLink>
           </span>
 
-          <span className="entry-date">{elem.published_at}</span>
+          <span className='entry-date'>{elem.published_at}</span>
 
-          <h2 className="entry-title">
+          <h2 className='entry-title'>
             <PostLink elem={elem}>{elem.title}</PostLink>
           </h2>
         </header>
---
+++ libs/intersection.js
@@ -0,0 +1,45 @@
+let listenerCallbacks = new WeakMap();
+
+let observer;
+
+function handleIntersections(entries) {
+  entries.forEach((entry) => {
+    if (listenerCallbacks.has(entry.target)) {
+      let cb = listenerCallbacks.get(entry.target);
+
+      if (entry.isIntersecting || entry.intersectionRatio > 0) {
+        observer.unobserve(entry.target);
+        listenerCallbacks.delete(entry.target);
+        cb();
+      }
+    }
+  });
+}
+
+function getIntersectionObserver() {
+  if (observer === undefined) {
+    observer = new IntersectionObserver(handleIntersections, {
+      root: null,
+      rootMargin: '100px',
+      threshold: 0.9,
+    });
+  }
+  return observer;
+}
+
+export default function watchIntersection(elem, callback) {
+  let target = elem.current;
+
+  if (!target) return;
+
+  let observer = getIntersectionObserver();
+
+  listenerCallbacks.set(target, callback);
+
+  observer.observe(target);
+
+  return () => {
+    listenerCallbacks.delete(target);
+    observer.unobserve(target);
+  };
+}
