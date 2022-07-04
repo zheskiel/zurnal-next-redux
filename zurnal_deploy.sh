e3e885f16c5cc7dc6957b54db183f87603ee6dc9
juanzheng <juanzheng@kkbox.com>
2022-07-04 13:44:47 +0800

    update script 16

--- zurnal_deploy.sh
+++ zurnal_deploy.sh
@@ -6,45 +6,27 @@ git pull
 
 yarn install || exit
 
-echo "Echo Build Dir 1...."
-
-BUILD_DIR="temp"
-NEXT_BUILD_DIR="temp"
-echo "$BUILD_DIR"
-echo "$NEXT_BUILD_DIR"
-
 yarn build
-
-echo "Echo Build Dir 2...."
-
-echo "$BUILD_DIR"
-echo "$NEXT_BUILD_DIR"
 
 if [ ! -d "temp" ]; then
   echo '\033[31m temp Directory not exists!\033[0m'
   exit 1;
 fi
 
-rm -rf .next
+# rm -rf .next
 
-if [ ! -d ".next" ]; then
-  mkdir .next
+#if [ ! -d ".next" ]; then
+#  mkdir .next
+#fi
+# mv temp/* .next
+
+if [ ! -d "test" ]; then
+  mkdir test
 fi
-mv temp/* .next
 
-echo "Echo Build Dir 3...."
-
-BUILD_DIR=".next"
-NEXT_BUILD_DIR=".next"
-
-echo "$BUILD_DIR"
-echo "$NEXT_BUILD_DIR"
-
-echo "Echo Build Dir 4...."
+mv temp/* test
 
 # yarn reload
-pm2 reload all --update-env
-pm2 reset all
 
 echo "Deploy done."
 
