aa9694b5620c7ff76ca5c59ec1f2a90c3bb15c99
idpelago <idpelago@gmail.com>
2022-07-10 01:10:15 +0800

    change react to preact

--- next.config.js
+++ next.config.js
@@ -24,6 +24,20 @@ module.exports = {
     GA_TRACKING: process.env.NEXT_PUBLIC_GA_ID,
   },
 
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+
+    return config;
+  },
+
   async rewrites() {
     return routes;
   },
