d8a93b3787c99e933270065829533eda321ea0e4
idpelago <idpelago@gmail.com>
2022-07-10 01:22:33 +0800

    fix disabled

--- Components/Pagination/index.js
+++ Components/Pagination/index.js
@@ -26,7 +26,7 @@ class Pagination extends Component {
         <li>
           <a
             className={isEligible ? "active" : ""}
-            disabled={isEligible}
+            disabled={!isEligible}
             onClick={(e) =>
               isEligible ? handlePagination(e, page) : e.preventDefault()
             }
@@ -61,7 +61,7 @@ class Pagination extends Component {
           <li key={index}>
             <a
               className={page == currentPage ? Styles.active : ""}
-              disabled={isEligible}
+              disabled={!isEligible}
               onClick={(e) =>
                 isEligible ? handlePagination(e, page) : e.preventDefault()
               }
