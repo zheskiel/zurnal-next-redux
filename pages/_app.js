cf8bc602c57d285c6f0eb970a5b588d674ec0232
juanzheng <juanzheng@kkbox.com>
2022-07-13 12:45:14 +0800

    comment out tracking

--- pages/_app.js
+++ pages/_app.js
@@ -77,7 +77,7 @@ const App = ({ Component, pageProps }) => {
 
   const component = (
     <>
-      {isProd && <TrackScripts />}
+      {/* {isProd && <TrackScripts />} */}
 
       <TopProgressBar />
 
