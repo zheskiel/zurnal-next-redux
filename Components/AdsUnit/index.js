da415013a9c7806bbcbbd902b0ad4ffcef69895c
idpelago <idpelago@gmail.com>
2022-08-20 00:22:42 +0800

    add AdsUnit wrapper

--- Components/AdsUnit/index.js
+++ Components/AdsUnit/index.js
@@ -3,14 +3,17 @@ import React, { Component } from "react";
 class Index extends Component {
   render() {
     return (
-      <ins
-        class="adsbygoogle"
-        style={{ display: "block" }}
-        data-ad-client="ca-pub-6983942794145260"
-        data-ad-slot="1294452039"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      />
+      <div className="ads-unit-wrapper">
+        <ins
+          key={Math.random()}
+          class="adsbygoogle"
+          style={{ display: "block" }}
+          data-ad-client="ca-pub-6983942794145260"
+          data-ad-slot="1294452039"
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        />
+      </div>
     );
   }
 }
