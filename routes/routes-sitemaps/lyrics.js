200456449e76f3cd41a23d4fe9c830ad3243c7d9
idpelago <idpelago@gmail.com>
2022-10-27 15:18:45 +0700

    fix app-lyrics sitemap typo

--- routes/routes-sitemaps/lyrics.js
+++ routes/routes-sitemaps/lyrics.js
@@ -6,7 +6,7 @@ module.exports = [
     destination: `${sitemapUrl}/app-lyrics/sitemap.xml`,
   },
   {
-    source: "/app-lyrics/sitemap-post-:id.xml",
-    destination: `${sitemapUrl}/app-lyrics/sitemap-post-:id.xml`,
+    source: "/app-lyrics/sitemap-lyric-:id.xml",
+    destination: `${sitemapUrl}/app-lyrics/sitemap-lyric-:id.xml`,
   },
 ];
